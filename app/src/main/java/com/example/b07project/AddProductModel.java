2c43dd2f80dba112d86913d8fa4abdba9b06e65a
rayma984 <moyraymond984@gmail.com>
2021-11-26 18:24:13 -0500

    RAYMOND got started on AddProductModel for owneraddproduct
    got started on the firebase stuff

--- app/src/main/java/com/example/b07project/AddProductModel.java
+++ app/src/main/java/com/example/b07project/AddProductModel.java
@@ -1,6 +1,14 @@
 package com.example.b07project;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddProductModel implements OwnerAddProductContract.OwnerAddProductModel{
+    //write to the database
+    void addProduct(Product p) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child()
+    }
 
 
 }
