7ee84256e93a2f75bad81c31649aab1876ceec4d
rayma984 <moyraymond984@gmail.com>
2021-11-26 00:43:31 -0500

    RAYMOND- Userstory 2: classes,interfaces, UI
    RAYMOND added classes/ interfaces [owner_add_product, OwnerAddProductContract] and UI for adding a product [activity_owner_add_product] so that owners can add products for USERSTORY2

---
+++ app/src/main/java/com/example/b07project/OwnerAddProductContract.java
@@ -0,0 +1,18 @@
+package com.example.b07project;
+
+public interface OwnerAddProductContract {
+
+    public interface OwnerAddProductModel{
+
+    }
+
+    public interface OwnerAddProductView{
+        public String getProductName();
+        public String getBrandName();
+        public String getPrice();
+    }
+
+    public interface OwnerAddProductPresenter{
+        public void checkProductInputs(String productName, String brandName, float price);
+    }
+}
---
+++ app/src/main/java/com/example/b07project/owner_add_product.java
@@ -0,0 +1,16 @@
+package com.example.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class owner_add_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_add_product);
+    }
+
+
+}
