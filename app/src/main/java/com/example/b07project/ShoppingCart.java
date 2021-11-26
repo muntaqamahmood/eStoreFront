00505ee63ec5c2963a3a19a867c4d1f6477ea909
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2021-11-26 09:46:11 -0500

    Created class ShoppingCart
    initial commit for class ShoppingCart for userstory 4, going to keep updating class ShoppingCart after setting other classes up.

---
+++ app/src/main/java/com/example/b07project/ShoppingCart.java
@@ -0,0 +1,51 @@
+package com.example.b07project;
+
+import java.util.ArrayList;
+
+public class ShoppingCart extends Order{
+    private int cartId;
+    private int userId;
+    private int totalPrice;
+    private int totalQuantity;
+    ArrayList<Product> added_to_cart = new ArrayList<Product>();
+    ArrayList<Product> removed_from_cart = new ArrayList<Product>();
+    ArrayList<Product> updated_in_cart = new ArrayList<Product>();
+    boolean added;
+    boolean removed;
+    boolean updated;
+    boolean empty;
+
+    public ShoppingCart(){
+        System.out.println("Your shopping cart is empty.");
+    }
+
+    public ShoppingCart(int cartId, int userId, int totalPrice, int totalQuantity) {
+        this.cartId = cartId;
+        this.userId = userId;
+        this.totalPrice = totalPrice;
+        this.totalQuantity = totalQuantity;
+    }
+    @Override
+    public void addItem(Product product) {
+        added_to_cart.addItem(product);
+    }
+
+    @Override
+    public void removeItem(Product product) {
+        removed_from_cart.removeItem(product);
+    }
+
+    public void updateCartadd(Product product) {
+        updated_in_cart.addItem(product);
+    }
+
+    public void updateCartremove(Product product) {
+        updated_in_cart.removeItem(product);
+    }
+
+    public void clearCart() {
+        added_to_cart.clear();
+        removed_from_cart.clear();
+        updated_in_cart.clear();
+    }
+}
