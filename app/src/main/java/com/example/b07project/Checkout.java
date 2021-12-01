a97e2cefe0efb6657c61672adbc0b5babb3b2d6f
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2021-11-30 22:15:45 -0500

    Initial ShoppingCart and Checkout Activities commit
    ShoppingCart and Checkout UI in progress

--- app/src/main/java/com/example/b07project/Checkout.java
+++ app/src/main/java/com/example/b07project/Checkout.java
@@ -16,23 +16,18 @@ public class Checkout extends ShoppingCart {
     }
     //getters and setters for retrieving and setting the values of the private variables
     public int getId() {return id;}
-
     public void setId(int id) {this.id = id;}
 
     public int getUserId() {return userId;}
-
     public void setUserId(int userId) {this.userId = userId;}
 
     public int getTotalPrice() {return totalPrice;}
-
     public void setTotalPrice(int totalPrice) {this.totalPrice = totalPrice;}
 
     public int getTotalQuantity() {return totalQuantity;}
-
     public void setTotalQuantity(int totalQuantity) {this.totalQuantity = totalQuantity;}
 
     public String getDate() {return date;}
-
     public void setDate(String date) {this.date = date;}
 
     @Override
---
+++ app/src/main/java/com/example/b07project/ShoppingCartActivity.java
@@ -0,0 +1,29 @@
+package com.example.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingCartActivity extends AppCompatActivity implements LoginContract.View{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public void result(boolean canLogin, Account account) {
+
+    }
+}
