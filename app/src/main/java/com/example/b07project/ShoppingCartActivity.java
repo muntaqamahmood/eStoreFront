23137ba7a918411722d49a74d02566b5c7bfc261
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2021-12-02 23:58:30 -0500

    Muntaqa finished UI for ShoppingCart and in progress with Checkout UI
    finished UI for ShoppingCart and in progress with Checkout UI, figuring out payment forms for checking out. Working on implementing ShoppingCart and PlaceOrder

--- app/src/main/java/com/example/b07project/ShoppingCartActivity.java
+++ app/src/main/java/com/example/b07project/ShoppingCartActivity.java
@@ -1,10 +1,23 @@
 package com.example.b07project;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShoppingCartActivity extends AppCompatActivity{
+    TextView quantity;
+    int totalQuantity = 1;
+    int totalPrice = 0;
+    ImageView detailedImg,addItem,removeItem;
+    TextView price,rating,description;
+    Button addToCart;
+    Toolbar toolbar;
+    //ViewAllModel viewAllModel = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
