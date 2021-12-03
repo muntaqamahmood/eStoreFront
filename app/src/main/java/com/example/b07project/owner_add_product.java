90d9d3d2b2dbbc0883ea8d1d61244f4bf907cad8
rayma984 <moyraymond984@gmail.com>
2021-12-02 23:57:47 -0500

    raymond- got rid of MVP model for userstory2
    didnt need that MVP stuff, everything is now in owner_add_product.
    Allah is testing me with this firebase stuff, but i can write some dummy values there for now

--- app/src/main/java/com/example/b07project/owner_add_product.java
+++ app/src/main/java/com/example/b07project/owner_add_product.java
@@ -8,31 +8,31 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class owner_add_product extends AppCompatActivity implements OwnerAddProductContract.OwnerAddProductView{
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    private OwnerAddProductContract.OwnerAddProductPresenter presenter;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class owner_add_product extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_add_product);
-
-        presenter = new OwnerAddProductPresenter(new AddProductModel(), this);
     }
 
-    @Override
     public String getProductName() {
         EditText editText = findViewById(R.id.txtProductName);
         return editText.getText().toString();
     }
 
-    @Override
     public String getBrandName() {
         EditText editText = findViewById(R.id.txtBrandName);
         return editText.getText().toString();
     }
 
-    @Override
     public String getPrice() {
         EditText editText = findViewById(R.id.txtPrice);
         return editText.getText().toString();
@@ -40,11 +40,61 @@ public class owner_add_product extends AppCompatActivity implements OwnerAddProd
 
     public void confirmButton(View view){
         StoreOwner owner = (StoreOwner) getIntent().getSerializableExtra("store_owner");
-        presenter.checkProductInputs(getProductName(),getBrandName(),getPrice(), owner);
+
+        String productName = getProductName();
+        String brandName = getBrandName();
+        String price = getPrice();
+
+        boolean isGood = checkProductInputs(productName,brandName,price);
+
+        if(isGood){
+            Product p = new Product(productName, brandName, Float.parseFloat(price));
+            fireBase(owner, p);
+        }
     }
 
     public void feedback(String msg){
         Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void fireBase(StoreOwner owner, Product p){
+
+        owner.addProduct(p);
+        String username = owner.getUsername();
+        /*** actual stuff
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("store owners").child(username).child("Products").setValue(owner.products);
+         ***/
+        ArrayList<String> testArray = new ArrayList<>();
+        testArray.add("a");
+        testArray.add("b");
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("store owners").child(username).child("Products").setValue(testArray);
+    }
+
+
+    /*** WAS FROM THE PRESENTER ***/
+    public boolean checkProductInputs(String product, String brand, String price){
+        if(product.equals("")||brand.equals("")||price.equals("")){
+            //one of the entries is empty
+            feedback("Entries cannot be empty!");
+            return false;
+        }
+        else if(!isPrice(price)) { //invalid price
+            feedback("Please enter a valid price!");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    //helper function to check if the input is an acceptable money value
+    private boolean isPrice(String str){
+        Pattern p = Pattern.compile("[1-9]\\d*.\\d\\d|0.\\d{1,2}|[1-9]\\d*");
+        Matcher m = p.matcher(str);
+        return m.matches();
     }
 }
