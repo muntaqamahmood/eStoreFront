37abd818165d5a0422c7ee6b4d06bf31da0fc726
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2021-12-04 16:05:17 -0500

    Muntaqa created the CustomerViewProduct page to view and add products to cart and added listView in its UI
    created the CustomerViewProduct page to view and add products to cart and added listView in its UI

---
+++ app/src/main/java/com/example/b07project/CustomerViewProduct.java
@@ -0,0 +1,14 @@
+package com.example.b07project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomerViewProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_view_product);
+    }
+}
