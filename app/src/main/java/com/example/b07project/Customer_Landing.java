d0a8c932a5df7f5d04448afdb29833719e8d196a
rayma984 <moyraymond984@gmail.com>
2021-12-03 19:36:32 -0500

    raymond- customer landing now welcomes the customer
    added some ~~pizazz~ to the landing page by making it so the textview displays the customer's name

--- app/src/main/java/com/example/b07project/Customer_Landing.java
+++ app/src/main/java/com/example/b07project/Customer_Landing.java
@@ -1,6 +1,7 @@
 package com.example.b07project;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +14,9 @@ public class Customer_Landing extends AppCompatActivity {
         setContentView(R.layout.activity_customer_landing);
         //get the customer from the previous activity
         customer = (Customer) getIntent().getSerializableExtra("account");
+
+        //display the customer's name on the textview
+        TextView txtCustomerName = findViewById(R.id.txtCustomerName);
+        txtCustomerName.setText(customer.getUsername());
     }
 }
