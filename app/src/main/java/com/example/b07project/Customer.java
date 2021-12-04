35c402a87228fc9f2f03d8909aba4a62741a03cd
rayma984 <moyraymond984@gmail.com>
2021-12-04 13:52:21 -0500

    updating customer class to use the new CustomerOrders class
    customers will now have a field that holds all their made orders

--- app/src/main/java/com/example/b07project/Customer.java
+++ app/src/main/java/com/example/b07project/Customer.java
@@ -1,5 +1,15 @@
 package com.example.b07project;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class Customer extends Account {
@@ -20,4 +30,35 @@ public class Customer extends Account {
     public void setUsername(String username){this.username = username;}
 
     public void setPassword(String password){this.password = password;}
+
+    //wipeAllOrders clears allOrders
+    //this is done on login so we can repopulate it without duplicating
+    public void wipeAllOrders(){allOrders.clear();}
+
+    //populateAllOrders will pull CustomerOrders from firebase and put it in allOrders
+    void populateAllorders() {
+        //read the database
+/**
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("customers").child(username).child("Products");
+        ref.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                //in case we cant get that value
+                if (!task.isSuccessful()) {
+                    Log.e("B07 Project", "Couldn't get data", task.getException());
+                } else {
+                    //go through every product under the store owner
+                    if (task.getResult().getChildren() != null) {
+                        for (DataSnapshot child : task.getResult().getChildren()) {
+                            Product p = child.getValue(Product.class);
+                            products.add(p);
+                        }
+                    }
+
+                }
+            }
+        });
+ **/
+    }
+
 }
