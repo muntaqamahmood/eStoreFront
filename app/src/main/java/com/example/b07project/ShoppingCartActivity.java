a5b24b2841de1a177df5d576f06f8ce8a3abab88
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2021-12-04 18:50:27 -0500

    committing ShoppingCart and to avoid merge

---
+++ app/src/main/java/com/example/b07project/ShoppingCartActivity.java
@@ -0,0 +1,15 @@
+package com.example.b07project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShoppingCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+
+    }
+}
