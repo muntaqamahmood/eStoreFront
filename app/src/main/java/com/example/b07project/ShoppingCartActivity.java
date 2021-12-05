4481bd2fdd9ad0143a1dcc71f340b482627c69b3
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2021-12-05 12:29:06 -0500

    Update ShoppingCartActivity.java

--- app/src/main/java/com/example/b07project/ShoppingCartActivity.java
+++ app/src/main/java/com/example/b07project/ShoppingCartActivity.java
@@ -1,27 +1,34 @@
 package com.example.b07project;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ShoppingCartActivity extends AppCompatActivity {
     private Customer customer;
     private StoreOwner storeOwner;
     private CustomerOrder order;
-    private ArrayList<String> products = new ArrayList<String>();
+    private ArrayList<Product> products = new ArrayList<Product>();
     private ListView lstView;
+    private Button orderBtn;
+    private TextView productName, productBrand, productPrice;
+
+    private String state = "Normal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,22 +37,58 @@ public class ShoppingCartActivity extends AppCompatActivity {
 
         customer = (Customer) getIntent().getSerializableExtra("customer");
         storeOwner = (StoreOwner)getIntent().getSerializableExtra("store_owner");
+        order = (CustomerOrder) getIntent().getSerializableExtra("order");
+        productName = (TextView)findViewById(R.id.txtProductName);
+        productBrand = (TextView)findViewById(R.id.txtBrandName);
+        productPrice = (TextView)findViewById(R.id.txtPrice);
+
         ArrayAdapter<Product> productsAdapter = new ArrayAdapter<Product>(this, android.R.layout.simple_list_item_1);
-        lstView = findViewById(R.id.productListView);
+        lstView = findViewById(R.id.cartListView);
         lstView.setAdapter(productsAdapter);
+        for(order.items : ){
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("store owners").child(storeOwner.getUsername()).child("Products");
-        reference.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        }
+        orderBtn = (Button)findViewById(R.id.orderButton);
+        orderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(!task.isSuccessful()){
-                    Log.e("B07 Project", "Data retrieve unsuccessful", task.getException());
+            public void onClick(View v) {
+                if(state.equals("Order successful!")||state.equals("Order ready for pickup!")){
+                    Toast.makeText(ShoppingCartActivity.this, "You can now pick up your order", Toast.LENGTH_LONG).show();
                 }else{
-                    if(task.getResult().getChildren()!=null){
-
-                    }
+                    addedToCart();
                 }
             }
         });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        checkOrder;
+    }
+    private void addedToCart(){
+        final DatabaseReference refToCartList = FirebaseDatabase.getInstance().getReference().child("Cart List");
+        final HashMap<String, Object>cartMap = new HashMap<>();
+        cartMap.put("name", productName.getText().toString());
+        cartMap.put("brand", productBrand.getText().toString());
+        cartMap.put("price", productPrice.getText().toString());
+        cartMap.put("orderNumber", productOrderNumber);
+
+        refToCartList.child("Customer view").child(customer.getUsername()).child("Products").child(productOrderNumber).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    refToCartList.child("")
+                }
+            }
+        });
+
+    }
+
+
+
+
+
+    private void getProductDetails(){
+
     }
 }
