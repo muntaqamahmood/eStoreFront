7414c883f860bb2a98c749e7d80c93f267575bbf
kiva45 <avik.toronto@gmail.com>
2021-12-05 00:14:57 -0500

    Avik updated ViewAllOrdersAcitivity to display all orders in a listview and a persistent listener to update in real time

--- app/src/main/java/com/example/b07project/ViewAllOrdersActivity.java
+++ app/src/main/java/com/example/b07project/ViewAllOrdersActivity.java
@@ -15,24 +15,27 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class ViewAllOrdersActivity extends AppCompatActivity {
 
-    private ArrayList<String> str_orders;
+    //private ArrayList<String> str_orders;
     private ArrayAdapter<String> ordersAdapter;
     private ListView listView;
-    private ArrayList<Order> orders;
+    private StoreOwner owner;
+//    private ArrayList<Order> orders;
 
-    private void items_to_str(ArrayList<Order> o){
-        str_orders = new ArrayList<String>();
-        for(Order i:o){
-            str_orders.add(i.toString());
-        }
-    }
+//    private void items_to_str(ArrayList<Order> o){
+//        str_orders = new ArrayList<String>();
+//        for(Order i:o){
+//            str_orders.add(i.toString());
+//        }
+//    }
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
@@ -40,9 +43,9 @@ public class ViewAllOrdersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_all_orders);
 
         listView = findViewById(R.id.listView);
-        StoreOwner owner = (StoreOwner) getIntent().getSerializableExtra("account");
+        owner = (StoreOwner) getIntent().getSerializableExtra("account");
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("store owners");
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("store owners");
 //        ValueEventListener listener = new ValueEventListener() {
 //            @Override
 //            public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -72,26 +75,26 @@ public class ViewAllOrdersActivity extends AppCompatActivity {
         ordersAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         listView.setAdapter(ordersAdapter);
 
-        ref.child(owner.getUsername()).child("Orders").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!(task.isSuccessful())){
-                    //error
-                }else{
-                    if (task.getResult().hasChildren()) {
-                        for (DataSnapshot child : task.getResult().getChildren()) {
-                            StoreOwner storeOwner = child.getValue(StoreOwner.class);
-                            if (storeOwner != null) {
-                                Log.i("test",storeOwner.getUsername());
-                                ordersAdapter.add(storeOwner.getUsername());
-                                //stores.add(storeOwner.getUsername());
-
-                            }
-                        }
-                    }
-                }
-            }
-        });
+//        ref.child(owner.getUsername()).child("Orders").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DataSnapshot> task) {
+//                if (!(task.isSuccessful())){
+//                    //error
+//                }else{
+//                    if (task.getResult().hasChildren()) {
+//                        for (DataSnapshot child : task.getResult().getChildren()) {
+//                            StoreOwner storeOwner = child.getValue(StoreOwner.class);
+//                            if (storeOwner != null) {
+//                                Log.i("test",storeOwner.getUsername());
+//                                ordersAdapter.add(storeOwner.getUsername());
+//                                //stores.add(storeOwner.getUsername());
+//
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        });
 //        ref.child(owner.getUsername()).child("Orders").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
 //            @Override
 //            public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -106,12 +109,37 @@ public class ViewAllOrdersActivity extends AppCompatActivity {
 
 //        items_to_str(orders);
 
+        display();
 
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("store owners").child(owner.username).child("Orders");
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                owner.wipeOrders();
+                owner.populateOrders();
+                display();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Context context = getApplicationContext();
+                Toast.makeText(context,"Error getting data", Toast.LENGTH_LONG).show();
+            }
+        };
         setUpListViewListener();
 
-
     }
+
+    private void display(){
+        ordersAdapter.clear();
+        if(!owner.orders.isEmpty()) {
+            for (CustomerOrder order : owner.orders) {
+                String toDisplay = "Ordered by " + order.customer + ", Order" + order.toString();
+                ordersAdapter.add(toDisplay);
+            }
+        }
+    }
+
     private void setUpListViewListener(){
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -120,14 +148,12 @@ public class ViewAllOrdersActivity extends AppCompatActivity {
                 DatabaseReference ref = FirebaseDatabase.getInstance().getReference("store owners");
                 Context context = getApplicationContext();
 
-                ref.child(owner.getUsername()).child("Orders");
-
-                orders.remove(position);
+                owner.orders.remove(position);
                 Toast.makeText(context,"Item Removed!",Toast.LENGTH_LONG).show();
 
-                ref.child(owner.getUsername()).child("Orders").setValue(orders);
+                ref.child(owner.getUsername()).child("Orders").setValue(owner.orders);
 
-                ordersAdapter.notifyDataSetChanged();
+//                ordersAdapter.notifyDataSetChanged();
                 return true;
             }
         });
