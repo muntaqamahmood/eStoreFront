c242d92870b224b3446c4c94aa6d3f63ec82edc1
kiva45 <avik.toronto@gmail.com>
2021-12-06 17:41:20 -0500

    Avik deleted methods that are never used for clarity

--- app/src/main/java/com/example/b07project/Customer_Landing.java
+++ app/src/main/java/com/example/b07project/Customer_Landing.java
@@ -23,15 +23,12 @@ public class Customer_Landing extends AppCompatActivity {
     }
     public void viewAllStores(View view){
         Intent intent = new Intent(this, viewStores.class);
-
         intent.putExtra("customer", customer); //customer is NOT null
         startActivity(intent);
     }
+
     //customer can see their past Orders
     public void seeOrders(View view){
-        //wipe and repopulate the Customer's orders to make sure theyre up to date
-        customer.wipeAllOrders();
-        customer.populateAllorders();
 
         Intent intent = new Intent(this,CustomerSeeOrders.class);
         intent.putExtra("account", customer);
